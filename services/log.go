e22d245de2d2e92b3ad5864211fb3b731546d8f4
Wayne Shen <wayne.shen@sarbeer.com>
2017-10-06 16:45:53 +0800

    [feature]log print whole line in console

--- services/log.go
+++ services/log.go
@@ -3,9 +3,31 @@ package services
 import (
 	"encoding/json"
 	"fmt"
+	"os"
+
+	"golang.org/x/crypto/ssh/terminal"
 )
 
-func Log(o interface{}) {
-	j, _ := json.Marshal(o)
-	fmt.Println(string(j))
+const isLogMode = true
+
+func Log(o ...interface{}) {
+	if !isLogMode {
+		return
+	}
+	br := getNewLine()
+	for _, v := range o {
+		j, _ := json.Marshal(v)
+		fmt.Println(br)
+		fmt.Println(string(j))
+	}
+	fmt.Println(br)
+}
+func getNewLine() string {
+	fd := int(os.Stdout.Fd())
+	termWidth, _, _ := terminal.GetSize(fd)
+	br := ""
+	for i := 0; i < termWidth; i++ {
+		br += "-"
+	}
+	return br
 }
