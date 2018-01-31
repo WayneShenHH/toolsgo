08073755c91a0c930d0c4ea6d2fc38bada0a23e4
Wayne Shen <wayne.shen@sarbeer.com>
2018-01-31 18:55:49 +0800

    [feature] add cron

---
+++ cmd/cron_task.go
@@ -0,0 +1,27 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/WayneShenHH/toolsgo/repository/repositoryimpl"
+	"github.com/WayneShenHH/toolsgo/services/schedulesvc"
+	"github.com/spf13/cobra"
+)
+
+var workerScheduleCmd = &cobra.Command{
+	Short: "Worker schedule",
+	Long:  "Schedured process",
+	Use:   "worker:cron",
+
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(cmd.Short)
+		fmt.Println(cmd.Long)
+		repo := repositoryimpl.New()
+		svc := schedulesvc.New(repo)
+		svc.Start()
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(workerScheduleCmd)
+}
---
+++ services/schedulesvc/schedule_service.go
@@ -0,0 +1,38 @@
+package schedulesvc
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/WayneShenHH/toolsgo/repository"
+	"github.com/robfig/cron"
+)
+
+type CronService struct {
+	repository.Repository
+}
+
+// New instence CronService
+func New(ctx repository.Repository) *CronService {
+	return &CronService{
+		Repository: ctx,
+	}
+}
+
+func (service *CronService) Start() {
+	scheduler := cron.New()
+	service.timerSchedule(scheduler)
+	scheduler.Start()
+	select {} //hang on main process
+}
+func (service *CronService) timerSchedule(scheduler *cron.Cron) {
+	fmt.Println("[scheduletask] schedule a task on the every quarter hour.")
+	//run at each hour on 00,15,30,45
+	spec := "0 0/15 * * * ?"
+	//run at each hour on 01,16,31,46
+	// spec = "* 1-59/15 * * * *"
+	// spec = "1-59/15 * * * * *"
+	scheduler.AddFunc(spec, func() {
+		fmt.Println("[timer]running at", time.Now())
+	})
+}
