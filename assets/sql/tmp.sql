8c93514c1d8c97ecd2c2935792f8110bb794e4bf
Wayne Shen <wayne.shen@sarbeer.com>
2018-12-06 20:27:40 +0800

    [refactor] add study article markdown

---
+++ assets/sql/tmp.sql
@@ -0,0 +1,26 @@
+1	Win Outright	1	soccer
+1	Win Outright	2	ice hockey
+1	Win Outright	3	basketball
+1	Win Outright	4	rugby union
+1	Win Outright	6	us football
+1	Win Outright	7	baseball
+1	Win Outright	8	handball
+1	Win Outright	16	snooker
+1	Win Outright	20	darts
+1	Win Outright	74	e-sports
+2	Relegation	1	soccer
+11	Finish Bottom	1	soccer
+12	Promotion	1	soccer
+14	Reach the Final	1	soccer
+15	Top 2 Finish	1	soccer
+16	Top 6 Finish	1	soccer
+19	Win Group 1(A)	1	soccer
+21	Win Group 3(C)	1	soccer
+24	Win Group 6(F)	1	soccer
+41	Win Without Barcelona and Real Madrid	1	soccer
+45	Win Group 12(L)	1	soccer
+46	Top 4 Finish	1	soccer
+87	Top Goalscorer	1	soccer
+88	Top 10 Finish	1	soccer
+89	Top 7 Finish	1	soccer
+93	To Stay Up	1	soccer
